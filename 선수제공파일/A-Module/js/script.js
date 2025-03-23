f5200f3ef6f6ff59938b3ab77c211cdd16150328
tlscksgur <12880924a@gmail.com>
2025-03-23 15:06:38 +0900

    Update script.js

--- 선수제공파일/A-Module/js/script.js
+++ 선수제공파일/A-Module/js/script.js
@@ -1,24 +1,26 @@
-fetch("./product.json")
+fetch("./js/product.json")
 .then(response => {
   return response.json()
 })
 
 .then(jdata =>{
   const allProduct = document.querySelector(".all-product")
+  allProduct.innerHTML = ""
 
   Object.keys(jdata.product).forEach(item => {
     const data = jdata.product[item]
 
     data.forEach(e => {
-      allProduct.innerHTML += `
-      <ul>
+      const ul = document.createElement('ul')
+      ul.innerHTML = `
         <li>${e.title}</li>
+        <li><img src="${e.img}" alt="${e.title}" class="product-img"></li>
         <li>${e.content}</li>
-        <li>${e.price}</li>
         <li>${e.delivery}</li>
+        <li>${e.price}</li>
         <li>${e.boon}</li>
-        <li>${e.point}</li>
-      </ul>`
+        <li>${e.point}</li>`
+      allProduct.appendChild(ul);
     });  
   });
 })
